92ad84b82a61675212645ad5b673b22206d6d60d
Vishal21121 <vs84077000@gmail.com>
2023-07-20 23:43:40 +0530

    added login endpoint
    - added middleware for login
    - added controller function for login
    - added route for login

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { validateRegister } from "../middleware/user.middleware.js";
+import { validateRegister, validateLogin } from "../middleware/user.middleware.js";
 import { validate } from "../validate.js"
-import { registerUser } from "../controller/user.controller.js";
+import { registerUser, loginUser } from "../controller/user.controller.js";
 
 const router = Router()
 
 router.route("/register").post(validateRegister(), validate, registerUser)
+router.route("/login").post(validateLogin(), validate, loginUser)
 
 export default router
