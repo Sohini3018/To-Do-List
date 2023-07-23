67d865e620a8ba3dd0eabf055ced8d22b5903b5e
Vishal21121 <vs84077000@gmail.com>
2023-07-23 13:54:32 +0530

    fixed some bugs and added pointer in signin button of login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -13,39 +13,40 @@ const Login = () => {
 
     const handlSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const response = await fetch("http://localhost:8000/api/v1/users/login", {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    "email": data.email,
-                    "password": data.password
+        if ((data.email && data.password))
+            try {
+                const response = await fetch("http://localhost:8000/api/v1/users/login", {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        "email": data.email,
+                        "password": data.password
+                    })
                 })
-            })
-            const val = await response.json()
-            if (val.data.statusCode === 200) {
-                localStorage.setItem("user", JSON.stringify(val.data.value))
-                toast.success(val.data.value.value)
-                setUserData(val.data.value)
-                setData({ email: "", password: "" })
-                navigate("/")
-            }else{
-                toast.error(val.data.value)
+                const val = await response.json()
+                if (val.data.statusCode === 200) {
+                    localStorage.setItem("user", JSON.stringify(val.data.value))
+                    toast.success(val.data.value.value)
+                    setUserData(val.data.value)
+                    setData({ email: "", password: "" })
+                    navigate("/")
+                } else {
+                    toast.error(val.data.value)
+                }
+            } catch (error) {
+                console.log("error in request for storing user", error);
+                toast.error(error.message)
             }
-        } catch (error) {
-            console.log("error in request for storing user", error);
-            toast.error(error.message)
-        }
-    }   
+    }
 
     useEffect(() => {
         console.log("logged in val", userData);
-        if (localStorage.getItem("user")) {
-            navigate("/")   
+        if (localStorage.getItem("user") !== null) {
             setUserData(data)
+            navigate("/")
         }
     }, [])
 
@@ -58,11 +59,11 @@ const Login = () => {
                 />
                 <div className="form-floating">
                     <label htmlFor="email" className='form-label'>Email address</label>
-                    <input type="email" className="form-input" id="email" placeholder="Enter your email id" value={data.email} onChange={(e) => setData(prev => ({ ...prev, "email": e.target.value }))}/>
+                    <input type="email" className="form-input" id="email" placeholder="Enter your email id" value={data.email} onChange={(e) => setData(prev => ({ ...prev, "email": e.target.value }))} />
                 </div>
                 <div className="form-floating">
                     <label htmlFor="password" className='form-label'>Password</label>
-                    <input type="password" className="form-input" id="password" placeholder="Enter you password" value={data.password} onChange={(e) => setData(prev => ({ ...prev, "password": e.target.value }))}/>
+                    <input type="password" className="form-input" id="password" placeholder="Enter you password" value={data.password} onChange={(e) => setData(prev => ({ ...prev, "password": e.target.value }))} />
                 </div>
                 <button className="btn" type="submit" id="signin" onClick={handlSubmit}>Sign in</button>
                 <div className='change-page'>
